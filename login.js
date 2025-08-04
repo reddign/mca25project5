46011953a49169d1b7a6f6e85663e0a9d0810d36
byentirc <castillohuitzil@etown.edu>
2025-08-04 11:25:13 -0400

    login page

---
+++ login.js
@@ -0,0 +1,16 @@
+function login(event){
+    event.preventDefault();
+
+    loginform = document.getElementById("login")
+    var username = loginform.elements["user"].value
+    var password = loginform.elements["password"].value
+
+
+    if(!username == "coolguy" || password == "123456789"){
+        alert("enter correct username and password")
+    }else{
+
+        loginform.submit();
+        // alert('Go away,'+ (username) + '! You are not a valid user.')
+    }
+}
