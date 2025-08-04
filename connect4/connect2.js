19f50cd8679795382a82727695d0e1a0d72d52a4
SharonChen3 <chen@etown.edu>
2025-08-04 11:34:50 -0400

    Connect 4-not done

---
+++ connect4/connect2.js
@@ -0,0 +1,15 @@
+function dropblock(event){
+    console.log("This allows user to drop block on click")
+}
+
+function drawblock(){
+    console.log("This draws a yellow circle")
+}
+
+function drawblock(){
+    console.log("This draws a red circle")
+}
+
+function win(){
+    console.log("Checks to see if there is four in a row")
+}
