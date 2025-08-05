a8e80d525bb9e922ea283288d509da8c223e8725
Beckett Mann <mann@etown.edu>
2025-08-05 10:34:51 -0600

    My commit

--- snake/snake.js
+++ snake/snake.js
@@ -2,3 +2,22 @@ let canvas = document.querySelector("canvas");
 let graphics = canvas.getContext('2d');
 let x=20;
 let y=100;
+let snakex = 50;
+let snakey = 50;
+let snakeWidth = 20, snakeHeight = 20;
+let FPS = 500;
+
+
+function animate(){
+    snake();
+    moveSnake();
+}
+function snake(){
+    graphics.fillStyle = "white"
+    graphics.fillRect(snakex,snakey,snakeWidth,snakeHeight);
+}
+function moveSnake(){
+    
+}
+
+window.setInterval(animate,FPS/1000)
