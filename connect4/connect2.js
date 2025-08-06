3985cedd1439f30815679876de9f4046203da19e
SharonChen3 <chen@etown.edu>
2025-08-06 09:22:38 -0400

    Edits

--- connect4/connect2.js
+++ connect4/connect2.js
@@ -2,8 +2,25 @@ let canvas = document.querySelector("canvas");
 const graphics = canvas.getContext("2d");
 
 drawboard();
+drawblock();
+
+// function instructions(){  //TODO Make the instructions work, not line broken
+//     graphics.beginPath();
+//     graphics.roundRect(10,200,300,200,10);
+//     graphics.fillStyle = '#dce7edcf';
+//     graphics.fill();
+//     graphics.strokeStyle = '#575c5f1a';
+//     graphics.lineWidth = 5;
+
+//     graphics.stroke();
 
 
+//     graphics.fillStyle = "black";
+//     graphics.strokeStyle = "black";
+//     let message = "Instructions:" + "\nPlayers choose yellow or red disks" + "\nThen, take turns and drop the discs into the grid.\n" +
+//     "Use strategy to block opponents while aiming to" + "\nbe the first player to get 4 in a row." 
+//     graphics.fillText(message,25,225);
+// }
 
 
 function dropblock(event){
@@ -12,7 +29,7 @@ function dropblock(event){
 
 function drawboard(){
     graphics.beginPath();
-    graphics.roundRect(365,100,530,430,10);
+    graphics.roundRect(370,5,530,430,10);
     graphics.fillStyle = '#94d5fdff';
     graphics.fill();
     graphics.strokeStyle = '#2da6f1ff';
@@ -21,12 +38,22 @@ function drawboard(){
     graphics.stroke();
 }
 
-function drawblock(){
-    console.log("This draws a yellow circle")
+//draws yellow circle
+function drawblocky(){
+    graphics.fillStyle = "yellow";
+    graphics.beginPath();
+    graphics.arc(x,y,20,0,Math.PI*2);
+    graphics.fill();
+    graphics.closePath();
 }
 
-function drawblock(){
-    console.log("This draws a red circle")
+//Draws red circle
+function drawblockr(){
+    graphics.fillStyle = "red";
+    graphics.beginPath();
+    graphics.arc(x,y,20,0,Math.PI*2);
+    graphics.fill();
+    graphics.closePath();
 }
 
 function checkwin(){
