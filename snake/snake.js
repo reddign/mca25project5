7ccdb504b93fd5b0b57c60dc5931bf1054085535
Beckett Mann <mann@etown.edu>
2025-08-07 11:22:25 -0600

    snake

--- snake/snake.js
+++ snake/snake.js
@@ -4,23 +4,51 @@ let x=20;
 let y=100;
 let snakex = 50;
 let snakey = 50;
+let tail = [];
 let snakeWidth = 20, snakeHeight = 20;
 let FPS = 500;
 let direction = "NONE";
+let lastDirection = "";
 let lives = 3;
+let pauseFrames= 0;
+let snakeColor="white";
+//Defines food. P0:X, P1: Y, P2: State
+let food=[50,50,1]
+console.log(food)
+console.log(Math.floor(10+Math.random()*370))
+ makeFood();
 function animate(){
-    clear();
-    snake();
-    console.log(snakex,snakey);
-    if(lives>0){
-        moveSnake();
-        checkSnake();   
+    // If pauseFrames are not active, run animation.
+    if (pauseFrames==0){
+        clear();
+        snake();
+        // console.log(snakex,snakey);
+        if(lives>0){
+            moveSnake();
+            checkSnake();
+            
+            drawFood();
+            if(checkSnakeEatsFood()){
+                console.log("Snake ate food.")
+                collectFood();
+            }
+            //collectFood();
+        }
+    }
+    // Otherwise, reduce pauseFrames. It will eventually go back to 0.
+    else{
+        pauseFrames-=1
     }
     
 }
 function snake(){
-    graphics.fillStyle = "white"
+    graphics.fillStyle = snakeColor
     graphics.fillRect(snakex,snakey,snakeWidth,snakeHeight);
+    //draw tail
+    for(let i=0; i<tail.length;i++){
+        graphics.fillStyle = "blue";
+        graphics.fillRect(tail[i][0],tail[i][1],snakeWidth,snakeHeight);
+    }
 }
 function clear(){
     graphics.fillStyle = "black"
@@ -37,34 +65,128 @@ function moveSnake(){
     }else if(direction=="RIGHT"){
         moveSnakeRight()
     }
+    
 }
 function moveSnakeDown(){
     snakey += 1;
+    for(let i=0; i<tail.length;i++){
+     //tail[i][0] -= 1
+     tail[i][1] += 1
+    
+    }
 }
 function moveSnakeUp(){
     snakey -= 1;
+    for(let i=0; i<tail.length;i++){
+    // tail[i][0] -= 1
+     tail[i][1] -= 1
+    
+    }
 }
 function moveSnakeRight(){
     snakex += 1;
+    for(let i=0; i<tail.length;i++){
+     tail[i][0] += 1
+    // tail[i][1] += 1
+    
+    }
+    
 }
 function moveSnakeLeft(){
     snakex -= 1;
+    for(let i=0; i<tail.length;i++){
+     tail[i][0] -= 1
+    // tail[i][1] -= 1
+    
+    }
 }
 function checkSnake(){
     if(snakex < 0 || snakey < 0 || snakex+snakeWidth > canvas.width || snakey+snakeHeight > canvas.height){
-        lives--;
-        pause();
-        snakex=30;
-        snakey=50;
+        resetSnake();
 
     }
 }
+function resetSnake(){
+    lives-=1;
+    direction="";
+    snakex=50;
+    snakey=50;
+    // Pauses the game for a bit.
+    pauseFrames=200;
+}
+
 function pause(){
     let x=1;
-    while(x<1000000000){
-        x++;
+}
+
+function makeFood(){
+    if (food[2]==0){
+        rngX=Math.floor(10+Math.random()*370)
+        rngY=Math.floor(10+Math.random()*370)
+        food=[rngX,rngY,1]
+        console.log(food)
     }
 }
+function drawFood(){
+    graphics.fillStyle="red"
+    graphics.fillRect(food[0],food[1],15,15)
+}
+function checkSnakeEatsFood(){
+    
+    var rectSnake = {
+        left: snakex,
+        top: snakey,
+        right: snakex + snakeWidth,
+        bottom: snakey + snakeHeight,
+        };
+
+    var rectFood = {
+        left: food[0],
+        top: food[1],
+        right: food[0] + 15,
+        bottom: food[1] + 15,
+        };
+    let ateFood = intersectRect(rectSnake, rectFood);
+    if(ateFood){
+        snakeColor="yellow";
+        return true;
+    }else{
+        return false;
+    }
+}
+function intersectRect(r1, r2) {
+  return !(r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top);
+}
+function collectFood(){
+    food[2]=0
+    makeFood();
+    growSnake();
+    
+}
+function growSnake(){
+
+    let newx = snakex;
+    let newy = snakey;
+    if(direction=="UP"){
+        newy += snakeHeight
+    }
+    if(direction=="DOWN"){
+        newy -= snakeHeight
+    }
+    if(direction=="LEFT"){
+        newx += snakeWidth
+    }
+    if(direction=="RIGHT"){
+        newx -= snakeWidth
+    }
+    let segment = [newx,newy];
+    tail.push(segment);
+    console.log(tail);
+}
+
 document.addEventListener("keydown",function(event){
     const key = event.key;
     const code = event.code;
@@ -84,4 +206,6 @@ document.addEventListener("keydown",function(event){
     }
 });
 
-window.setInterval(animate,FPS/1000)
+
+
+window.setInterval(animate,FPS/1000);
